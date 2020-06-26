a0a4a46f9c6e4046f2638377c08124a8a528f634
Jonathan Castro <jonathan.castro@whitejaguars.com>
2020-06-26 16:59:59 -0600

    Add dashboard pending info

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -26,14 +26,14 @@ class DashboardController extends Controller
             ->select('users.id as id', 'users.name as name', 'users.lastName as lName1', 'users.motherLastName as lName2',
                     'users.dateBirth as bDate', 'genders.description as gender', 'users.email as email',
                     'personaldata.phoneNumber as phone', 'provinces.name as province', 'cantons.name as canton',
-                    'districts.name as district', 'personaldata.address as address')->get();
+                    'districts.name as district', 'personaldata.address as address')->paginate(10);
 
         $pendingData = DB::table('users')
             ->join('genders', 'genders.id', 'users.gender')
             ->leftJoin('personaldata', 'users.id', 'personaldata.userId')
             ->whereNull('personaldata.userId')
             ->select('users.id as id', 'users.name as name', 'users.lastName as lName1', 'users.motherLastName as lName2',
-                'users.dateBirth as bDate', 'genders.description as gender', 'users.email as email')->get();
+                'users.dateBirth as bDate', 'genders.description as gender', 'users.email as email')->paginate(10);
 
         if (Auth::user()->isMember == commonData::ADMIN_CODE)
             return view('dashboard', compact('userData', 'pendingData'));
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -74,10 +74,22 @@
                                         @endforeach
                                     </tbody>
                                 </table>
+                                {{ $userData->links() }}
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <br>
+    <br>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-auto">
+                <div class="card">
                     <div class="card-header">{{__('commons.pendingData')}}</div>
                     <div class="card-body">
                         <div class="card-title text-center"></div>
@@ -127,6 +139,7 @@
                                     @endforeach
                                     </tbody>
                                 </table>
+                                {{ $pendingData->links() }}
                             </div>
                         </div>
                     </div>
